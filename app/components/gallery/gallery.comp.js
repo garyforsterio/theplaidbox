248ef9a22f9e13bc97dfd3d4d93e903da911f5e1
vineet0713 <vineet0713@gmail.com>
2016-09-06 16:19:48 -0700

    Update gallery.comp.js

--- app/components/gallery/gallery.comp.js
+++ app/components/gallery/gallery.comp.js
@@ -13,37 +13,25 @@
 
     GalleryController.$inject = [];
     function GalleryController() {
-        this.items = [
-            {
-                name: "Template 1",
-                price: "£100000",
-                seller: "George"
-            },
-            {
-                name: "Template 2",
-                price: "£1000",
-                seller: "Mike"
-            },
-            {
-                name: "Template 1",
-                price: "£100000",
-                seller: "George"
-            }];
+        this.items = [];
         this.previews = [
             {
-                name: "Template 1",
-                price: "£100000",
-                seller: "George"
+                name: "Lands End Panorama",
+                price: "$74.99",
+                seller: "George Smith"
+                img: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=&url=http%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F440941961349201920%2FWZKquS-P.jpeg&psig=AFQjCNFF0ihTpDQut-hJgGJIzNSzB9_7mQ&ust=1473288381708933&cad=rjt"
             },
             {
-                name: "Template 2",
-                price: "£1000",
-                seller: "Mike"
+                name: "Golden Gate Sunny Shot",
+                price: "$99.99",
+                seller: "Michael Gump"
+                img: "http://maiden-voyage-travel.com/wp-content/uploads/2013/01/Golden-Gate-Bridge-San-Fran.jpg"
             },
             {
-                name: "Template 1",
-                price: "£100000",
-                seller: "George"
+                name: "Pier 39 Long Exposure",
+                price: "$54.99",
+                seller: "Frank Daniels"
+                img: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=&url=http%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F440941961349201920%2FWZKquS-P.jpeg&psig=AFQjCNFF0ihTpDQut-hJgGJIzNSzB9_7mQ&ust=1473288381708933&cad=rjt"
             },
         ];
         this.$onInit = function() { };
