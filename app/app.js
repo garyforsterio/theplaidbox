2a3288b37c536fd8f58e6f7ca1bff1571933fe58
Gary Forster <gary.forster@animustech.com>
2016-09-06 15:01:30 -0700

    feature - add angular material

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('app', [
   'ngRoute',
+  'ngMaterial'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
