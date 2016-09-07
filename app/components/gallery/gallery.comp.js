9074f4d6eaa77b2f970a6e9a1fd73e4a7fdcbf12
vineet0713 <vineet0713@gmail.com>
2016-09-06 17:04:26 -0700

    Update gallery.comp.js

--- app/components/gallery/gallery.comp.js
+++ app/components/gallery/gallery.comp.js
@@ -34,14 +34,32 @@
                 img: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=&url=http%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F440941961349201920%2FWZKquS-P.jpeg&psig=AFQjCNFF0ihTpDQut-hJgGJIzNSzB9_7mQ&ust=1473288381708933&cad=rjt"
             },
         ];
-        this.$onInit = function() { };
-        this.$onChanges = function(changesObj) { };
-        this.$onDestroy = function() { };
-    }
-    function preview(name) {
-
-    }
-    function purchase(name, price) {
         
+        var stripeHandler = $window.StripeCheckout.configure({
+ -            key: "pk_test_tE36CdGxRaB0wywdMHlhzBkU",
+ -            // billingAddress: true,
+ -            // zipCode: true,
+ -            allowRememberMe: false,
+ -            token: function (token) {
+ -                var toast = $mdToast.simple()
+ -                    .textContent('Successfully purchased')
+ -
+ -                $mdToast.show(toast);
+ -
+ -                //Send http request to lambda
+ -            }
+ -        });
+ -
+ -        this.purchase = function(name, price) {
+ -            stripeHandler.open({
+ -                name: name,
+ -                amount: price
+ -            });
+ -        };
+ -
+ -
+ -        this.$onInit = function () { };
+ -        this.$onChanges = function (changesObj) { };
+ -        this.$onDestory = function () { };
     }
 })();
